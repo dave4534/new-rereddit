252c288e84a1d272fddc98da1630b4fb7ced1436
Dave <uDave@Main.local>
2016-12-27 16:25:06 +0200

    About to start client side work

--- routes/index.js
+++ routes/index.js
@@ -20,7 +20,7 @@ router.post('/posts', function(req, res, next) {
 
 router.param('post', function(req, res, next, id) {
   var query = Post.findById(id);
-  console.log('got here')
+  console.log('got here');
 
   query.exec(function (err, post){
     if (err) { return next(err); }
@@ -31,14 +31,33 @@ router.param('post', function(req, res, next, id) {
   });
 });
 
-router.post('/posts/:post/comments/', function(req, res, next) {
-    // res.send(req.post);
+router.post('/posts/:post/comments', function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post._id;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
 });
 
 router.get('/posts', function(req, res, next){
   //find is a mongoose query that is predefined
   Post.find(function(error, posts){
     res.send(posts);
+  });
+});
+
+router.get('/posts/:post', function(req, res, next){
+  //find is a mongoose query that is predefined
+  req.post.populate('comments', function(err, post){
+    res.send(post);
   });
 });
 
